e608937f3745a6ae6c0b8c6fa5e91f27bb22b8de
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-16 22:37:57 +0530

    updated

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>This is home component</h1>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        name:'Home'
+    },
+})
+</script>
