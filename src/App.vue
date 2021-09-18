0ac89901510a1a91b4ac9460b5503103d9f9f8d8
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-18 08:38:29 +0530

    conditional render

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  <Home />
+  
   <Home data="You are there" msg="hi from You" />
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,8 +4,11 @@
     <h2>{{ data }}</h2>
     <h2>{{ msg }}</h2>
     <button v-on:mouseover="hello('hello call')">Call Hello Function</button>
-    
 
+    <!-- conditional rendering in vue js -->
+    <h1 v-if="show">conditional rendering in vue js</h1>
+    <h1 v-else>conditional rendering else block</h1>
+    <button v-on:click="display">Toggle Element</button>
   </div>
 </template>
 <script>
@@ -15,9 +18,16 @@ export default {
     data: String,
     msg: String,
   },
+  data() {
+    return { show: true };
+  },
   methods: {
     hello(data) {
-      alert(data)
+      alert(data);
+    },
+    display(){
+      this.show=!this.show
+
     },
   },
 };
