938af3dbc9e71d4aa5e3613d935c6357b62cb15c
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-18 20:02:08 +0530

    form validation

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     <!-- <Home6 v-bind:users="users"/> -->
     <!-- <h2>{{title}}</h2>
     <Home7 v-on:changeTitle="changeTitle($event)"/> -->
-    <Home10/>
+    <Home11/>
   </div>
 </template>
 
@@ -21,12 +21,12 @@
 //import Home2 from './components/Home2.vue';
 //import Users from './components/Users.vue';
 //import Home3 from './components/Home3.vue';
-import Home10 from "./components/Home10.vue";
+import Home11 from "./components/Home11.vue";
 
 export default {
   name: "App",
   components: {
-    Home10,
+    Home11,
     // HelloWorld,
     //Home,
     //Home2,
---
+++ src/components/Home11.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>form validation</h1>
+        
+        <p v-if="error.length">
+            <b>Please correct the following errors</b>
+            <ul>
+                <li v-for="e in error" :key="e.id">
+                    {{e}}
+                </li>
+            </ul>
+        </p>
+        
+        <form @submit="login">
+            <input type="text" placeholder="enter your name" v-model="name"><br><br>
+            <input type="password" placeholder="enter your password" v-model="password"><br><br>
+            <button type="submit">Login</button>
+
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name:'Home11',
+    data(){
+        return{
+            error:[],
+            name:null,
+            password:null
+        }            
+    },
+    methods:{
+        login(e){
+            this.error=[];
+            if(this.name && this.password){
+                console.warn("no error");
+            }
+            if(!this.name){
+                this.error.push("user name is required");
+            }
+            if(!this.password){
+                this.error.push("password is required")
+            }
+            console.warn("Hello",this.error);
+            e.preventDefault();
+        }
+    }
+}
+</script>
