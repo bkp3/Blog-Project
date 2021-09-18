aa433845ab75810feca6cb310c76a0dd37a3156d
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-18 14:34:35 +0530

    props

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   <!-- <Home2/> -->
   <!-- <users/> -->
 <!-- <Home3/> -->
-<Home5/>
+<Home6 v-bind:users="users"/>
 
 </template>
 
@@ -17,7 +17,7 @@
 //import Home2 from './components/Home2.vue';
 //import Users from './components/Users.vue';
 //import Home3 from './components/Home3.vue';
-import Home5 from './components/Home5.vue';
+import Home6 from './components/Home6.vue';
 
 export default {
   name: "App",
@@ -27,8 +27,18 @@ export default {
     //Home2,
     //Users,
     //Home3,
-    Home5
+    Home6
   },
+  data(){
+      return{
+        users:[
+          {name:"peter",email:"peter@gmail.com"},
+          {name:"harry",email:"harry@gmail.com"},
+          {name:"rocky",email:"rocky@gmail.com"},
+          {name:"mak",email:"mak@gmail.com"},
+        ]
+      }
+  }
 };
 </script>
 
---
+++ src/components/Home6.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>props in vue js</h1>
+        <!-- <h1>{{name}}</h1> -->
+
+        <h1 v-for="user in users" :key="user.name">
+            {{user.name}} --> {{user.email}}
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    name:'Home6',
+    props:['users']
+}
+</script>
