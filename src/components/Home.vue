43c00f52d2fe1c5d74051b590000669bb12e19ba
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-18 08:22:24 +0530

    event

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,14 +3,22 @@
     <h1>This is home component</h1>
     <h2>{{ data }}</h2>
     <h2>{{ msg }}</h2>
+    <button v-on:mouseover="hello('hello call')">Call Hello Function</button>
+    
+
   </div>
 </template>
 <script>
 export default {
   name: "Home",
-  props:{
-      data:String,
-      msg:String
-  }
+  props: {
+    data: String,
+    msg: String,
+  },
+  methods: {
+    hello(data) {
+      alert(data)
+    },
+  },
 };
 </script>
