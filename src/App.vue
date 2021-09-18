4c8f7d86fc903c2e5518fd0bde1097b967710788
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-18 14:53:55 +0530

    props child to parent component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  
-  <!-- <Home data="You are there" msg="hi from You" /> -->
-  <!-- <Home2/> -->
-  <!-- <users/> -->
-<!-- <Home3/> -->
-<Home6 v-bind:users="users"/>
+  <div>
+    <h1 style="color:red;"><u>Welcome to Vue.js World</u></h1>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Vue.js hello World" /> -->
 
+    <!-- <Home data="You are there" msg="hi from You" /> -->
+    <!-- <Home2/> -->
+    <!-- <users/> -->
+    <!-- <Home3/> -->
+    <!-- <Home6 v-bind:users="users"/> -->
+    <h2>{{title}}</h2>
+    <Home7 v-on:changeTitle="changeTitle($event)"/>
+  </div>
 </template>
 
 <script>
-
 // import HelloWorld from './components/HelloWorld.vue'
 //import Home from "./components/Home.vue";
 //import Home2 from './components/Home2.vue';
 //import Users from './components/Users.vue';
 //import Home3 from './components/Home3.vue';
-import Home6 from './components/Home6.vue';
+import Home7 from "./components/Home7.vue";
 
 export default {
   name: "App",
@@ -27,17 +30,28 @@ export default {
     //Home2,
     //Users,
     //Home3,
-    Home6
+    Home7,
   },
+  // data(){
+  //     return{
+  //       users:[
+  //         {name:"peter",email:"peter@gmail.com"},
+  //         {name:"harry",email:"harry@gmail.com"},
+  //         {name:"rocky",email:"rocky@gmail.com"},
+  //         {name:"mak",email:"mak@gmail.com"},
+  //       ]
+  //     }
+  // }
+
   data(){
-      return{
-        users:[
-          {name:"peter",email:"peter@gmail.com"},
-          {name:"harry",email:"harry@gmail.com"},
-          {name:"rocky",email:"rocky@gmail.com"},
-          {name:"mak",email:"mak@gmail.com"},
-        ]
-      }
+    return{
+      title:"props tutorial"
+    }
+  },
+  methods:{
+    changeTitle(title){ 
+      this.title=title
+    }
   }
 };
 </script>
---
+++ src/components/Home7.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Send props child to parent</h1>
+
+        <button v-on:click="updateTitle">Update Parent Title</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'Home7',
+    methods:{
+        updateTitle(){
+            //console.warn('Hello from title')
+            this.$emit('changeTitle','Parent Updated')
+        }
+    }
+}
+</script>
