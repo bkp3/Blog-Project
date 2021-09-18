83aa02fc0d00785ab77ed95cb5819261bf2a059d
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-18 20:25:40 +0530

    beforeMount and Mounted life cycle methods

---
+++ src/components/Home13.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1> beforeMount and Mounted life cycle methods</h1>
+        <h1>{{name}}</h1>
+
+    </div>
+</template>
+<script>
+export default {
+    name:"Home13",
+    data(){
+        return{
+            name:"Mukesh Kumar"
+        }
+    },
+    beforeMount(){
+        console.log("beforeMount",this.$el);
+    },
+    mounted(){
+        console.log("mounted",this.$el);
+    }
+}
+</script>
