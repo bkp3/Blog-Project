bdca38a5b7d51edd714f8d582f958bc9bed6b717
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-19 06:45:10 +0530

    beforedestroy and destroyed

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
     <!-- <Home6 v-bind:users="users"/> -->
     <!-- <h2>{{title}}</h2>
     <Home7 v-on:changeTitle="changeTitle($event)"/> -->
-    <Home13/>
+    <Home14/>
   </div>
 </template>
 
@@ -21,12 +21,12 @@
 //import Home2 from './components/Home2.vue';
 //import Users from './components/Users.vue';
 //import Home3 from './components/Home3.vue';
-import Home13 from "./components/Home13.vue";
+import Home14 from "./components/Home14.vue";
 
 export default {
   name: "App",
   components: {
-    Home13,
+    Home14,
     // HelloWorld,
     //Home,
     //Home2,
---
+++ src/components/Home14.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>beforeDestroy and destroyed life cycle methods</h1>
+        <h1>{{name}}</h1>
+
+
+    </div>
+</template>
+<script>
+export default {
+    name:'Home14',
+    data(){
+        return{
+            name:"Home Component"
+        }
+    },
+    // beforeDestroy(){
+    //     console.warn("before destroy")
+    // },
+    // destroyed(){
+    //     console.warn("destroyed")
+    // }
+}
+</script>
